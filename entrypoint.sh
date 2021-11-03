bcf347dbbcd81dc992589ce5199936624ad5e226
Steve Francia <steve.francia@gmail.com>
2021-11-03 14:00:27 -0400

    Add support for target

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,15 @@ if [ -z "${FIREBASE_PROJECT}" ]; then
     exit 1
 fi
 
+if [ -z "${FIREBASE_TARGET}" ]; then
 firebase deploy \
     -m "${GITHUB_REF} (${GITHUB_SHA})" \
     --project ${FIREBASE_PROJECT} \
     --only hosting
+    exit 0
+fi
+
+firebase deploy \
+    -m "${GITHUB_REF} (${GITHUB_SHA})" \
+    --project ${FIREBASE_PROJECT} \
+    --only hosting:${FIREBASE_TARGET} 
